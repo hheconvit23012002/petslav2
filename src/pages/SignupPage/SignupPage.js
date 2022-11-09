f6356e411d8feeb5783c866f1af945390bb8ed89
buonthuong <danhhuan23012002@gmail.com>
2022-11-09 19:50:00 +0700

    fix signup

--- src/pages/SignupPage/SignupPage.js
+++ src/pages/SignupPage/SignupPage.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Signup from "./../../components/signup/Signup"
-
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 function SignupPage(){
+    const navigate = useNavigate();
+    useEffect(() => {
+        if(sessionStorage.getItem("token") !== null){
+            navigate("/")
+        }
+    },[navigate])
     return (
         <Signup />
     )
